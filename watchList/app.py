a871962e0051764cfb1ab4cd11821ac660a1c1b1
吴宇轩 <13806632+wu-yuxuan-blog@user.noreply.gitee.com>
2023-12-08 23:10:57 +0800

    学习了基本的Jinja2渲染

--- watchList/app.py
+++ watchList/app.py
@@ -1,11 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import url_for
 from markupsafe import escape
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Welcome to My WatchList!<br><h1>Here is a title</h1><img src="http://helloflask.com/totoro.gif">'
 
 @app.route('/user/<name>')
 def user_page(name):
@@ -13,7 +10,24 @@ def user_page(name):
 
 @app.route('/test')
 def test_url_for():
-    print(url_for('hello'))
+    print(url_for('index'))
     print(url_for('user_page',name='xuan'))
     print(url_for('test_url_for',num=2))
     return '<h1>这是一个测试页面</h1>'
+
+name = '轩初号机'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+@app.route('/')
+def index():
+    return render_template('index.html',name=name,movies=movies)
